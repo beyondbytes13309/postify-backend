496dc012490534ff87af0400dfc0cebb42e95242
beyondbytes13309 <emmarthedev@gmail.com>
2025-07-19 00:21:28 +0500

    feat(posts): add secure post creation with validation and error handling

--- controllers/postControlller.js
+++ controllers/postControlller.js
@@ -1,5 +1,42 @@
-const createPost = (Post, User, req, res) => {
-    return res.status(200).json({ code: '69', data: 'Only for testing purposes' })
+const createPost = async (Post, User, req, res) => {
+    const { postText } = req.body
+    if (!postText) {
+        return res.status(400).json({ code: '010', data: 'Invalid data'})
+    }
+
+    const newPost = new Post({
+        authorID: req.user._id,
+        postText: postText
+    })
+
+    try {
+        await newPost.save()
+        return res.status(201).json({ code: '015', data: 'Post created successfully'})
+    } catch(e) {
+        if (e.name == 'ValidationError') {
+            if (e.errors?.postText?.kind == 'maxlength') {
+                return res
+                    .status(400)
+                    .json({
+                    code: "022",
+                    data: "Post is longer than maximum allowed length",
+                });
+            }
+            if (e.errors?.postText?.kind == 'minlength') {
+                return res
+                    .status(400)
+                    .json({
+                    code: "022",
+                    data: "Post is shorter than minimum allowed length",
+                });
+            }
+
+            return res.status(400).json({ code: '022', data: 'Incorrect data'})
+
+        } else {
+            return res.status(500).json({ code: '550', data: 'Unexpected error occured!'})
+        }
+    }
 }
 
 module.exports = { createPost }
